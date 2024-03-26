e22f0c2a18bb2f846d25dfb56b7ef1b25eb072bc
smiljkovic <nikola.smiljkovic@gmail.com>
2024-03-26 10:16:55 +0000

    - new IP address

--- starter-kit/config/vtiger.php
+++ starter-kit/config/vtiger.php
@@ -13,7 +13,7 @@ return [
     */
 
     'vtiger_api_endpoint' => 'localhost/modules/CustomerPortal/api.php',
-    'vtiger_public_api_endpoint' => 'http://192.168.56.101/',
+    'vtiger_public_api_endpoint' => 'crm.nikola.com',
     'STRIPE_PUBLISHABLE_KEY' => 'pk_test_51OKJS7L9id96rr8Dlu0FFWeV7yy1NBJpN567P12x6sW1fi3aaohzIx6OkNp4DvbUifHrGrT8FN6bMEwgXxtMns5m001WaEfNe3', //<replace-with-your-publishable-key>
     'STRIPE_SECRET_KEY' => 'sk_test_51OKJS7L9id96rr8DqGTW0vq7PLlxnydYt07G8yXtmMWwJA9L8pKLDZpBnsUtAOXyg7kIF7FLROXRAheCklQqj07B00q0WoUS5S', //<replace-with-your-secret-key>
     //STATIC_DIR=../../client/html
